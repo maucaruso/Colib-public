2d18a2be588993c533b3a6f1d2b9df411572a628
Maurici Caruso <carusojr@outlook.com>
2020-06-05 23:08:10 -0300

    Ajustes Widget de perfil nas listagens

--- routes/index.js
+++ routes/index.js
@@ -26,11 +26,15 @@ const User = mongoose.model('users');
 
 // Biblioteca
     router.get('/biblioteca', (req, res) => {
-        Book.find().sort({date: 'desc'}).then((books) => {
-            res.render('site/library', {books: books.map(book => book.toJSON())}); 
-        }).catch((err) =>{
-            req.flash('error_msg', 'Houve um erro ao listar os livros, por favor, tente novamente'); 
-        }); 
+        User.find().then((users) => {
+            Book.find().sort({date: 'desc'}).then((books) => {
+                res.render('site/library', {users: users.map(user => user.toJSON()), books: books.map(book => book.toJSON())}); 
+            }).catch((err) =>{
+                req.flash('error_msg', 'Houve um erro ao listar os livros, por favor, tente novamente'); 
+            }); 
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro ao carregar a página, por favor, tente novamente'); 
+        });
     });
 
     router.get('/biblioteca/:id/:slug', (req, res) => {
@@ -44,11 +48,15 @@ const User = mongoose.model('users');
 
 // Artigos
     router.get('/artigos', (req, res) => {
-        Post.find().sort({date: 'desc'}).then((posts) => {
-            res.render('site/articles', {posts: posts.map(post => post.toJSON())}); 
-        }).catch((err) =>{
-            req.flash('error_msg', 'Houve um erro ao listar os artigos, por favor, recarregue a página.'); 
-        }); 
+        User.find().then((users) => {
+            Post.find().sort({date: 'desc'}).then((posts) => {
+                res.render('site/articles', {users: users.map(user => user.toJSON()), posts: posts.map(post => post.toJSON())}); 
+            }).catch((err) =>{
+                req.flash('error_msg', 'Houve um erro ao listar os artigos, por favor, recarregue a página.'); 
+            }); 
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro ao carregar a página, por favor, tente novamente'); 
+        });
     });
 
     router.get('/artigos/:id/:slug', (req, res) => {
