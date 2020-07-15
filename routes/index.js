c47c9453b3e87ecbe2ea28ab1838aa82a866ef79
Maurici Caruso <carusojr@outlook.com>
2020-07-14 22:49:40 -0300

    listagens em ordem cronologica

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ const User = mongoose.model('users');
 // Home
     router.get('/', (req, res) => {
         User.find({user_status: 1}).then((users) => {
-            Book.find({visibility_status: 1}).sort({date: 'desc'}).limit(12).then((books) => {
-                Post.find({visibility_status: 1}).sort({date: 'desc'}).limit(12).then((posts) => { 
+            Book.find({visibility_status: 1}).sort({date: 'asc'}).limit(12).then((books) => {
+                Post.find({visibility_status: 1}).sort({date: 'asc'}).limit(12).then((posts) => { 
                     res.render('site/index', {users: users.map(user => user.toJSON()), books: books.map(book => book.toJSON()), posts: posts.map(post => post.toJSON()), pageInfo: 'home'}); 
                 }).catch((err) =>{
                     req.flash('error_msg', 'Houve um erro ao carregar a página, por favor, tente novamente'); 
@@ -27,7 +27,7 @@ const User = mongoose.model('users');
 // Biblioteca
     router.get('/biblioteca', (req, res) => {
         User.find({user_status: 1}).then((users) => {
-            Book.find({visibility_status: 1}).sort({date: 'desc'}).then((books) => {
+            Book.find({visibility_status: 1}).sort({date: 'asc'}).then((books) => {
                 res.render('site/library', {users: users.map(user => user.toJSON()), books: books.map(book => book.toJSON()), pageInfo: 'library'}); 
             }).catch((err) =>{
                 req.flash('error_msg', 'Houve um erro ao listar os livros, por favor, tente novamente'); 
@@ -54,7 +54,7 @@ const User = mongoose.model('users');
 // Artigos
     router.get('/artigos', (req, res) => {
         User.find({user_status: 1}).then((users) => {
-            Post.find({visibility_status: 1}).sort({date: 'desc'}).then((posts) => {
+            Post.find({visibility_status: 1}).sort({date: 'asc'}).then((posts) => {
                 res.render('site/articles', {users: users.map(user => user.toJSON()), posts: posts.map(post => post.toJSON()), pageInfo: 'articles'}); 
             }).catch((err) =>{
                 req.flash('error_msg', 'Houve um erro ao listar os artigos, por favor, recarregue a página.'); 
@@ -137,8 +137,8 @@ const User = mongoose.model('users');
     router.get('/profile/:nickname', (req, res) => {
         User.findOne({nickname:req.params.nickname, access: 1, user_status: 1}).lean().then((user_detail) => {
             if(user_detail){
-                Book.find({user: user_detail._id, visibility_status: 1}).sort({date: 'desc'}).then((books) => {
-                    Post.find({user: user_detail._id, visibility_status: 1}).sort({date: 'desc'}).then((posts) => { 
+                Book.find({user: user_detail._id, visibility_status: 1}).sort({date: 'asc'}).then((books) => {
+                    Post.find({user: user_detail._id, visibility_status: 1}).sort({date: 'asc'}).then((posts) => { 
                         res.render('site/profile-details', {user_detail: user_detail, books: books.map(book => book.toJSON()), posts: posts.map(post => post.toJSON()), pageInfo: 'profile-details'}); 
                     }).catch((err) =>{
                         req.flash('error_msg', 'Houve um erro ao carregar a página, por favor, tente novamente'); 
@@ -162,8 +162,8 @@ const User = mongoose.model('users');
     router.get('/search/', (req, res) => {
         var search_terms = req.query.find;
         User.find({user_status: 1}).then((users) => {
-            Book.find({ $or:[ {'name':{$regex: search_terms, $options: "i"}}, {'author':{$regex: search_terms, $options: "i"}}, {'hashtags':{$regex: search_terms, $options: "i"}} ], visibility_status: 1}).sort({date: 'desc'}).then((books) => {
-                Post.find({ $or:[ {'title':{$regex: search_terms, $options: "i"}}, {'hashtags':{$regex: search_terms, $options: "i"}} ], visibility_status: 1}).sort({date: 'desc'}).then((posts) => {
+            Book.find({ $or:[ {'name':{$regex: search_terms, $options: "i"}}, {'author':{$regex: search_terms, $options: "i"}}, {'hashtags':{$regex: search_terms, $options: "i"}} ], visibility_status: 1}).sort({date: 'asc'}).then((books) => {
+                Post.find({ $or:[ {'title':{$regex: search_terms, $options: "i"}}, {'hashtags':{$regex: search_terms, $options: "i"}} ], visibility_status: 1}).sort({date: 'asc'}).then((posts) => {
                     res.render('site/search-result', {books: books.map(book => book.toJSON()), posts: posts.map(post => post.toJSON()), pageInfo: 'search-result'}); 
                 }).catch((err) =>{
                     req.flash('error_msg', 'Houve um erro ao carregar a busca.'); 
